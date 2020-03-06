dded8aac09e37e1a35d7aed0bc23338967006e12
ikramhasib007 <ikramhasib007@gmail.com>
2020-03-06 14:59:11 +0600

    project initiated

---
+++ src/screens/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+
+  return <View>
+    <Text>I am from home screen</Text>
+  </View>
+}
+
+const styles = StyleSheet.create({
+  
+})
